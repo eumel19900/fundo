2e2849ad9ee48163e9a1a5eebfb270327738261f
Robert Wermuth <robert.wermuth@googlemail.com>
2026-02-21 16:51:15 +0100

    Created session singleton

--- fundo/App.xaml.cs
+++ fundo/App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using fundo.core;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -46,6 +47,9 @@ namespace fundo
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            // Initialize application-wide session and shared services
+            Session.Initialize();
+
             _window = new MainWindow();
             // keep a static reference to the strongly-typed MainWindow
             MainWindowInstance = _window as MainWindow;
---
+++ fundo/core/Session.cs
@@ -0,0 +1,51 @@
+using System;
+using fundo;
+using fundo.core;
+using fundo.core.Search.Index;
+
+namespace fundo.core
+{
+    /// <summary>
+    /// Application-wide session singleton
+    /// </summary>
+    internal sealed class Session : IDisposable
+    {
+        private static Session? _current;
+
+        // Public static getter for the singleton session instance
+        public static Session Instance
+        {
+            get
+            {
+                if (_current == null)
+                {
+                    throw new InvalidOperationException("Session is not initialized. Call Session.Initialize() at app startup.");
+                }
+
+                return _current;
+            }
+        }
+
+
+        private Session()
+        {
+           
+        }
+
+        /// <summary>
+        /// Initializes the singleton session instance. Should be called once from App at startup.
+        /// </summary>
+        public static void Initialize()
+        {
+            if (_current == null)
+            {
+                _current = new Session();
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
